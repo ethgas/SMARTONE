5e57bbe959289e10059fc12c118de0808fa8fea2
ethgas <33361524+ethgas@users.noreply.github.com>
2017-11-04 10:02:33 +1100

    Create smartoneTEST1.sol

---
+++ smartoneTEST1.sol
@@ -0,0 +1,105 @@
+FOR TESTING PURPOSES ONLY
+
+
+contract airDrop {
+    function verify(address _address, bytes32 _secret) public constant returns (bool _status);
+}
+
+contract SMART1 {
+    string public constant symbol = "SMT1";
+
+    string public constant name = "Smart One";
+
+    uint public constant decimals = 8;
+
+    uint _totalSupply = 12000000 * 10 ** decimals;
+
+    address owner;
+
+    address airDropVerify;
+
+    uint public airDropAmount;
+
+    uint32 public airDropHeight;
+
+    mapping (address => bool) public airDropMembers;
+
+    mapping (address => uint) accounts;
+
+    mapping (address => mapping (address => uint)) allowed;
+
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+
+    function SmartOne() public {
+        owner = msg.sender;
+        accounts[owner] = _totalSupply;
+        Transfer(address(0), owner, _totalSupply);
+    }
+
+    function totalSupply() public constant returns (uint __totalSupply) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address _account) public constant returns (uint balance) {
+        return accounts[_account];
+    }
+
+    function allowance(address _account, address _spender) public constant returns (uint remaining) {
+        return allowed[_account][_spender];
+    }
+
+    function transfer(address _to, uint _amount) public returns (bool success) {
+        require(_amount > 0 && accounts[msg.sender] >= _amount);
+        accounts[msg.sender] -= _amount;
+        accounts[_to] += _amount;
+        Transfer(msg.sender, _to, _amount);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint _amount) public returns (bool success) {
+        require(_amount > 0 && accounts[_from] >= _amount && allowed[_from][msg.sender] >= _amount);
+        accounts[_from] -= _amount;
+        allowed[_from][msg.sender] -= _amount;
+        accounts[_to] += _amount;
+        Transfer(_from, _to, _amount);
+        return true;
+    }
+
+    function approve(address _spender, uint _amount) public returns (bool success) {
+        allowed[msg.sender][_spender] = _amount;
+        Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+    
+    function airDropJoin(bytes32 _secret) public payable returns (bool _status) {
+        // Checkout airdrop conditions and eligibility
+        require(!airDropMembers[msg.sender] && airDrop(airDropVerify).verify(msg.sender, _secret) && airDropHeight > 0 && airDropAmount > 0 && accounts[owner] >= airDropAmount);
+        // Transfer amount
+        accounts[owner] -= airDropAmount;
+        accounts[msg.sender] += airDropAmount;
+        airDropMembers[msg.sender] = true;
+        Transfer(owner, msg.sender, airDropAmount);
+        airDropHeight--;
+        // Return extra amount to sender
+        if (msg.value > 0) {
+            msg.sender.transfer(msg.value);
+        }
+        return true;
+    }
+
+    function airDropSetup(address _contract, uint32 _height, uint _units) public returns (bool _status) {
+        require(msg.sender == owner);
+        airDropVerify = _contract;
+        airDropHeight = _height;
+        airDropAmount = _units * 10 ** decimals;
+        return true;
+    }
+    
+        function withdrawFunds(uint _amount) public returns (bool _status) {
+        require(msg.sender == owner && _amount > 0 && this.balance >= _amount);
+        owner.transfer(_amount);
+        return true;
+    }
+}
